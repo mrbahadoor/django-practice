187e314b40e9e46eabad68c9f4703e37a07bef24
mrbahadoor <mrbahadoor@gmail.com>
2023-11-09 21:52:44 +0400

    Route with parameter and app view

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from employees.models import Employee
+from django.http import HttpResponse #,Http404
 
 # Create your views here.
+def employee_details(request, pk): #pk is primary key
+    employee = get_object_or_404(Employee, pk=pk)
+
+    context = {
+        'employee' : employee,
+    }
+
+    return render(request, 'employee_detail.html', context)
+
+
+    # try:
+    #     employee = Employee.objects.get(pk=pk)
+    #     print(employee)
+    # except:
+    #     raise Http404
+
